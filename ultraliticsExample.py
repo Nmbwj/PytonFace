b6b9789d5866766b3488e7021f32706fded695f4
Nmbwj <84346757+Nmbwj@users.noreply.github.com>
2024-07-19 14:13:26 +0300

    Test on webcam

--- ultraliticsExample.py
+++ ultraliticsExample.py
@@ -1,19 +1,24 @@
+import torch
 from ultralytics import YOLO
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print("Device named in torch: {}".format(device))
+
 # Create a new YOLO model from scratch
-model = YOLO("yolov8n.yaml")
+#model = YOLO("yolov8n.yaml")
 
 # Load a pretrained YOLO model (recommended for training)
 model = YOLO("yolov8n.pt")
 
 # Train the model using the 'coco8.yaml' dataset for 3 epochs
-results = model.train(data="coco8.yaml", epochs=3)
+#results = model.train(data="coco8.yaml", epochs=3)
 
 # Evaluate the model's performance on the validation set
-results = model.val()
+#results = model.val()
 
 # Perform object detection on an image using the model
-results = model("https://ultralytics.com/images/bus.jpg")
+results = model(source="0",show=True)
 
 # Export the model to ONNX format
-success = model.export(format="onnx")
+#success = model.export(format="onnx")
